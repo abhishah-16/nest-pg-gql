3c85ab06eced2be8cc3e501b1d8e2aed4a7b8d8e
abhishah <abhishah0961@gmail.com>
2022-09-29 15:58:26 +0530

    Added: Typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,15 +6,28 @@ import { AppService } from './app.service';
 import { EmployeeModule } from './employee/employee.module';
 import { ApolloDriver } from '@nestjs/apollo';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core'
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [EmployeeModule,GraphQLModule.forRoot({
-    playground: false,
-    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-    driver: ApolloDriver,
-    plugins: [ApolloServerPluginLandingPageLocalDefault],
-  })],
+  imports: [
+    EmployeeModule,
+    GraphQLModule.forRoot({
+      playground: false,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      driver: ApolloDriver,
+      plugins: [ApolloServerPluginLandingPageLocalDefault],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'nest_typeorm',
+      synchronize: true
+      
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
